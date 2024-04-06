fdc1a12dfdb82a4b72dcf600d449bc396dc44b69
hojinlee <infohojin@gmail.com>
2024-04-06 19:39:57 +0900

    first

---
+++ src/Http/Livewire/JinyProfileServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+namespace Jiny\Profile;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\View\Compilers\BladeCompiler;
+use Livewire\Livewire;
+//use Laravel\Fortify\Fortify;
+
+use Illuminate\Routing\Router;
+
+class JinyProfileServiceProvider extends ServiceProvider
+{
+    private $package = "jiny-profile";
+    public function boot()
+    {
+        // 모듈: 라우트 설정
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', $this->package);
+
+        // 데이터베이스
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        // 설정파일 복사
+        $this->publishes([
+            __DIR__.'/../config/setting.php' => config_path('jiny/profile/setting.php'),
+        ]);
+
+
+    }
+
+    public function register()
+    {
+        /* 라이브와이어 컴포넌트 등록 */
+        $this->app->afterResolving(BladeCompiler::class, function () {
+       
+            
+        });
+
+    }
+
+}
